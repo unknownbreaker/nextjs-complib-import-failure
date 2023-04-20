bb5e6b86214e0a5ae9a5f1d82edd2f8df1268d05
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2023-04-20 17:24:17 -0400

    WIP: Find workaround to Global CSS import error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
 import dynamic from 'next/dynamic';
+import '@/styles/globals.css';
+import '../../public/styles/krg.scss';
 
 // Components
 const KrgProvider = dynamic(() => import('@kargo/component-library-react.krg-shared').then((mod) => mod.KrgProvider), {
