ed260174441ce8460a14e1b3e55945bec372cdba
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2023-04-20 13:04:38 -0400

    Add TopNav to top of all pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import TopNav from '../components/TopNav';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <TopNav />
         <Main />
         <NextScript />
       </body>
