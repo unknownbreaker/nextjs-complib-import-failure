b17641a1070be089abd9abf7d3e70a1e8445c4bc
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2023-04-20 17:23:35 -0400

    Use dynamic imports for krg components

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,4 +1,7 @@
-import KrgHeader from '@kargo/component-library-react.krg-header';
+import dynamic from 'next/dynamic';
+const KrgHeader = dynamic(() => import('@kargo/component-library-react.krg-header'), {
+  ssr: false,
+});
 
 export default function TopNav(props: any) {
   return <KrgHeader />;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
+import dynamic from 'next/dynamic';
+
+// Components
+const KrgProvider = dynamic(() => import('@kargo/component-library-react.krg-shared').then((mod) => mod.KrgProvider), {
+  ssr: false,
+});
+
+// Types
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <KrgProvider>
+      <Component {...pageProps} />
+    </KrgProvider>
+  )
 }
