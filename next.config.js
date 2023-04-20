111e1e69899b2867b654b96d58231b8e51da26e8
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2023-04-20 17:25:04 -0400

    Remove sass configs

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: ['src', 'node_modules/@kargo/component-library-react.krg-shared/dist/styles'].map(dir => path.join(__dirname, dir)),
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
