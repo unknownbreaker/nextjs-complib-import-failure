c1f40edc819464236a1f703e098cb277ceef8320
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2023-04-20 17:59:27 -0400

    Add next-transpile-modules package

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const withTM = require('next-transpile-modules')([
+  '@kargo/component-library-react.krg-shared'
+]);
+
 const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withTM(nextConfig);;
