d472bfcac64c337c489530421a379744f571aab4
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2023-04-20 13:52:01 -0400

    Apply sass loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: ['src', 'node_modules/@kargo/component-library-react.krg-shared/dist/styles'].map(dir => path.join(__dirname, dir)),
+  },
 }
 
 module.exports = nextConfig
